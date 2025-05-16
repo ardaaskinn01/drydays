27d35110b2f0e8514e34af8ea16eed2848ac6616
ardaaskinn01 <ardaaskindm@gmail.com>
2025-05-17 01:13:21 +0300

    last updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,20 +10,10 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Firebase debug logları (isteğe bağlı)
-    FirebaseConfiguration.shared.setLoggerLevel(.debug)
-
-    // Firebase başlatma
-    FirebaseApp.configure()
-
-    // Flutter eklentileri
     GeneratedPluginRegistrant.register(with: self)
-
-    // Bildirim izinleri (flutter_local_notifications için)
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
