d2bea8f7076eed727725d7e3d650b6ffe5313089
ardaaskinn01 <ardaaskindm@gmail.com>
2025-05-17 00:35:15 +0300

    last updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,16 +2,28 @@ import UIKit
 import Flutter
 import FirebaseCore
 import flutter_local_notifications
+import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Firebase debug logları (isteğe bağlı)
+    FirebaseConfiguration.shared.setLoggerLevel(.debug)
+
+    // Firebase başlatma
     FirebaseApp.configure()
+
+    // Flutter eklentileri
     GeneratedPluginRegistrant.register(with: self)
+
+    // Bildirim izinleri (flutter_local_notifications için)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
